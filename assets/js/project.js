421ad06e30b7cfed2117ca45e1cbf3904c150bce
arubinofaux <arubinofaux@gmail.com>
2016-07-15 19:17:36 -0400

    crypto currency page updates

--- assets/js/project.js
+++ assets/js/project.js
@@ -39,20 +39,12 @@ $(document).ready(function(){
     },
     play: {
       active: true,
-        // [boolean] Generate the play and stop buttons.
-        // You cannot use your own buttons. Sorry.
       effect: "slide",
-        // [string] Can be either "slide" or "fade".
       interval: 7000,
-        // [number] Time spent on each slide in milliseconds.
       auto: true,
-        // [boolean] Start playing the slideshow on load.
       swap: false,
-        // [boolean] show/hide stop and play buttons
       pauseOnHover: true,
-        // [boolean] pause a playing slideshow on hover
       restartDelay: 2500
-        // [number] restart delay on inactive slideshow
     },
     effect: {
       slide: {
